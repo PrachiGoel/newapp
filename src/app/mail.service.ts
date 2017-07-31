07f0404ea792bdca23e25e407bdd43df363a8aab
prachi <prachi.goel@comparepolicy.com>
2017-07-31 18:28:18 +0530

    sort

---
+++ src/app/mail.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MailService {
+
+  message= [
+    `you have got mail`,
+`hivjfjf`, `jhhuhh`
+];
+  constructor() { }
+
+}
